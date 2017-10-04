53a8992550b7bc29b6c59cf35d17a17d190d5faf
ruixue <ruixuewu@163.com>
2017-10-05 00:47:08 +0800

    更改

--- RemoveSpace.js
+++ RemoveSpace.js
@@ -1,15 +1,13 @@
 /*清除字符串前后的空格*/
 function removeSpace(string) {
     var begin = 0;
-    while (begin < string.length, 'string'[begin] === ' ') {
+    while (begin < string.length && string[begin] === ' ') {
         begin++;
     }
-    if ('string'[string.length - 1] === ' ') {
-        var end = -1;
-        while (string.length + end > 0, 'string'.charAt[string.length - 1 + end] === ' ') {
-            end--;
-        }
+    var end = string.length - 1;
+    while (end > begin && string[end] === ' ') {
+        end--;
     }
-    newString = string.slice(begin, end);
+    newString = string.slice(begin, end + 1);
     return newString;
 }
