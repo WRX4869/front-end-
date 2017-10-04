89e8b111badbfbca39f83ca7adca3098506df6e8
ruixue <ruixuewu@163.com>
2017-10-04 22:23:37 +0800

    删去数组判断

--- deleteDuplicates.js
+++ deleteDuplicates.js
@@ -1,15 +1,11 @@
 /*如何消除一个数组里面重复的元素？*/
-function deleteDuplicates(array) {
-    if (array.constructor === Array) {
-        for (var i = 0; i < array.length; i++) {
-            for (var j = i + 1; j < array.length - 1; j++) {
+    function deleteDuplicates(array) {
+        for (var i = 0; i < array.length - 1; i++) {
+            for (var j = i + 1; j < array.length; j++) {
                 if (array[i] === array[j]) {
-                    array.splice(j, 1);
-                } else {
-                    continue;
+                    array.splice(j, 1);//
                 }
             }
         }
         return array;
     }
-}
