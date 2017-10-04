3b736a8e606acb2c40dadcab9db6fd8e43bd3eba
ruixue <ruixuewu@163.com>
2017-10-04 23:45:08 +0800

    更改

--- RemoveSpace.js
+++ RemoveSpace.js
@@ -1,16 +1,14 @@
 /*清除字符串前后的空格*/
 function removeSpace(string) {
     var begin = 0;
-    do {
+    while (begin < string.length, 'string'[begin] === ' ') {
         begin++;
     }
-    while (begin < string.length, 'string'.charAt(begin) === '/0');
-    if ('string'.charAt(string.length - 1) === '/0') {
+    if ('string'[string.length - 1] === ' ') {
         var end = -1;
-        do {
+        while (string.length + end > 0, 'string'.charAt[string.length - 1 + end] === ' ') {
             end--;
         }
-        while (string.length + end > 0, 'string'.charAt(string.length - 1 + end) === '/0');
     }
     newString = string.slice(begin, end);
     return newString;
