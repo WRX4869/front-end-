e93443da3d3c422a211c9401f2b1092856256c81
kucinl <961635991@qq.com>
2017-10-08 01:27:43 +0800

    ...
    nothing to say

--- JavaScript/practise/extract-parameters.js
+++ JavaScript/practise/extract-parameters.js
@@ -1,20 +1,9 @@
 /*有这样一个URL：http://item.taobao.com/item.htm?a=1&b=2&c=&d=xxx&e，请写一段JS程序提取URL中的各个GET参数(参数名和参数个数不确定)，将其按key-value形式返回到一个json结构中，如{a:'1', b:'2', c:'', d:'xxx', e:undefined}*/
-function extractParameters(string) {
-    let reg = /[?&](\w)(=)?([^&=])?/g;
-    let get = new Object();
-    while (match = reg.exec(string))
-        if (match[2] === "=" && match[3] !== undefined) {
-            get.name = match[1];
-            get.number = match[3];
-            //console.log(match[1] + ":" + "'" + match[3] + "'");
-        } else if (match[2] === "=" && match[3] === undefined) {
-            get.name = match[1];
-            get.number = ' ';
-            //console.log(match[1] + ":" + "' '");
-        } else if (match[2] !== "=" && match[3] === undefined) {
-            //console.log(match[1] + ":" + match[3]);
-            get.name = match[1];
-            get.number = 'undefined';
-        }
-    return 
+function extractParameters(url) {
+    let reg = /[?&](\w+)(=([^&=]*))?/g;
+    let parameters = {};
+    while (match = reg.exec(url)) {
+        parameters[match[1]] = match[3]
+    }
+    return parameters;
 } 
